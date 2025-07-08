4f75a7d03cfd0874fbba4db7c91066fd38ea880b
Dudu <eduardodpp07@gmail.com>
2025-07-08 11:49:49 -0300

    Criando os models

--- database/migrations/2025_07_08_143151_create_cart_items_table.php
+++ database/migrations/2025_07_08_143151_create_cart_items_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cartItems', function (Blueprint $table) {
+        Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
             $table->integer('quantity');
             $table->decimal('unitPrice', 10, 2);
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cartItems');
+        Schema::dropIfExists('cart_items');
     }
 };
