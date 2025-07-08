31e8e7ed351d2ed882db1a20129ef33283ce7c6c
Dudu <eduardodpp07@gmail.com>
2025-07-08 10:36:41 -0300

    Primeiras migrations, users, addresses, orders.

---
+++ database/migrations/2025_07_08_125205_create_torders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('address_id');
+            $table->dateTime('orderDate');
+            $table->enum('status', ['PENDING', 'PROCESSING', 'SHIPPED', 'COMPLETED', 'CANCELED']);
+            $table->decimal('totalAmount',10,2);
+            $table->foreignId('user_id')
+            ->constrained('users')
+            ->onDelete('cascade');
+            $table->foreignId('address_id')
+            ->constrained('addresses')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
