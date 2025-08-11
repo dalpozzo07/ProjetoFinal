2bd55151f9695f4f99f2a294e2eb62e215d4df99
Dudu <eduardodpp07@gmail.com>
2025-08-11 07:51:15 -0300

    Adicionand autenticação para ver se tem carrinho

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,13 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
+
 
 class CartController extends Controller
 {
     public function cart(Request $request)
     {
 
-      $cart = Cart::all();
+      $user = Auth::user();
+
+      if(!$user) {
+        return response()->json([
+            'status' => false,
+            'message' => 'Usuário não encontrado'
+        ], 404);
+      }
+      
+      $cart = Cart::where('user_id', Auth::user()->id)->first();
       return response()->json($cart);
        
     }
